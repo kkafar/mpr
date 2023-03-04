e2b3be8de714545ebe0f9c2de12ec9c1dc4268ad
Kacper Kafara <kacperkafara@gmail.com>
2023-03-04 15:38:42 +0100

    Add file for task1

--- lab-01/playground/src/main.c
+++ lab-01/playground/src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char * argv[]) {
   int rank, size;
 
 
-	MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
