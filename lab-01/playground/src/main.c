836c0181e772e4918103a638250bc97a7f30b015
Kacper Kafara <kacperkafara@gmail.com>
2023-03-05 11:33:03 +0100

    Create stub experiments

--- lab-01/playground/src/main.c
+++ lab-01/playground/src/main.c
@@ -65,15 +65,56 @@ void print_process_info(void) {
   printf("Hostname: %s, Process: %d, Total Process Count: %d, Config codename: %d\n", g_hostname.data, g_rank, g_size, CONFIG_CODE);
 }
 
-
-
 // pomiary przepustowości w zależności od długości komunikatów
 void experiment_1(void) {
-  // printf("
+  printf("Starting EXP 1 on process: %d\n", g_rank);
+  printf("Ending   EXP 1 on process: %d\n", g_rank);
 }
 
 // pomiary opóźnienia (przepustowość przy małym komunikacie)
 void experiment_2(void) {
+  printf("Starting EXP 2 on process %d\n", g_rank);
+
+  const int cping = 0;
+  const int cpong = 1;
+  const char communication_type[] = "Standard";
+
+  char payload = 'a';
+
+  const int iteration_count = ITERATION_COUNT;
+
+  double start_time, end_time;
+
+  // Synchronizujemy wszystkie procesy tego komunkatora
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  start_time = MPI_Wtime();
+  for (int i = 0; i < iteration_count; ++i) {
+    if (g_rank == cping) {
+
+      // Zaczynamy mierzyć czas
+      // double send_time = MPI_Wtime();
+
+      // Ping zaczyna
+      MPI_Send(&payload, 1, MPI_BYTE, cpong, 0, MPI_COMM_WORLD);
+      MPI_Recv(&payload, 1, MPI_BYTE, cpong, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      // double receive_time = MPI_Wtime();
+
+      // printf("Ping received data for %d time\n", i);
+      
+    } else if (g_rank == cpong) {
+      MPI_Recv(&payload, 1, MPI_BYTE, cping, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      // printf("Pong received data for %d time\n", i);
+      MPI_Send(&payload, 1, MPI_BYTE, cping, 0, MPI_COMM_WORLD);
+    } else {
+      printf("Dunno what happened\n");
+    }
+  }
+  end_time = MPI_Wtime();
+
+  printf("Time elapsed on %d: %lf\n", g_rank, end_time - start_time);
+  printf("Ending   EXP 2 on process: %d\n", g_rank);
 }
 
 
@@ -82,46 +123,13 @@ int main(int argc, char * argv[]) {
   assert((init_global_state() == true) && "Global state initialized");
   print_process_info();
 
-  // const int cping = 0;
-  // const int cpong = 1;
-  // const char communication_type[] = "Standard";
-  //
-  // char payload = 'a';
-  //
-  // const int iteration_count = ITERATION_COUNT;
-  // 
-  // double start_time, end_time;
-  //
-  // // Synchronizujemy wszystkie procesy tego komunkatora
-  // MPI_Barrier(MPI_COMM_WORLD);
-  //
-  // start_time = MPI_Wtime();
-  // for (int i = 0; i < iteration_count; ++i) {
-  //   if (g_rank == cping) {
-  //
-  //     // Zaczynamy mierzyć czas
-  //     // double send_time = MPI_Wtime();
-  //
-  //     // Ping zaczyna
-  //     MPI_Send(&payload, 1, MPI_BYTE, cpong, 0, MPI_COMM_WORLD);
-  //     MPI_Recv(&payload, 1, MPI_BYTE, cpong, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //
-  //     // double receive_time = MPI_Wtime();
-  //
-  //     // printf("Ping received data for %d time\n", i);
-  //     
-  //   } else if (g_rank == cpong) {
-  //     MPI_Recv(&payload, 1, MPI_BYTE, cping, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //     // printf("Pong received data for %d time\n", i);
-  //     MPI_Send(&payload, 1, MPI_BYTE, cping, 0, MPI_COMM_WORLD);
-  //   } else {
-  //     printf("Dunno what happened\n");
-  //   }
-  // }
-  // end_time = MPI_Wtime();
-  //
-  // printf("Time elapsed on %d: %lf\n", g_rank, end_time - start_time);
-  //
+  MPI_Barrier(MPI_COMM_WORLD);
+  experiment_1();
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  experiment_2();
+
+
   MPI_Finalize();
   return 0;
 }
