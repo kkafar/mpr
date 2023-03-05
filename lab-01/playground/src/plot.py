5f6ddb5aa7b3ba925b7b136274588e05ba55db48
Kacper Kafara <kacperkafara@gmail.com>
2023-03-05 16:16:27 +0100

    Change metaparameters

--- lab-01/playground/src/plot.py
+++ lab-01/playground/src/plot.py
@@ -5,13 +5,41 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    throughput_single_node_data = pd.read_csv('./throughput-single-node.csv')
-    throughput_two_nodes_data = pd.read_csv('./throughput-two-nodes.csv')
+    throughput_single_node_data = pd.read_csv('./throughput-single-node-all.csv')
+    throughput_two_nodes_data = pd.read_csv('./throughput-two-nodes-all.csv')
 
-    # delay_single_node_data = pd.read_csv('./delay-single-node.csv')
-    # delay_two_nodes_data = pd.read_csv('./delay-two-nodes.csv')
+    delay_single_node_data = pd.read_csv('./delay-single-node-all.csv')
+    delay_two_nodes_data = pd.read_csv('./delay-two-nodes-all.csv')
 
     print(throughput_single_node_data)
+    print(throughput_two_nodes_data)
+    print(delay_single_node_data)
+    print(delay_two_nodes_data)
 
+    throughput_single_node_std_x = throughput_single_node_data[throughput_single_node_data['type'] == 'std']['throughput']
+    throughput_two_nodes_std_x = throughput_two_nodes_data[throughput_two_nodes_data['type'] == 'std']['throughput']
+    throughput_single_node_buff_x = throughput_single_node_data[throughput_single_node_data['type'] == 'buff']['throughput']
+    throughput_two_nodes_buff_x = throughput_two_nodes_data[throughput_two_nodes_data['type'] == 'buff']['throughput']
+
+    throughput_single_node_std_y = throughput_single_node_data[throughput_single_node_data['type'] == 'std']['msgsize']
+
+    plt.plot(
+        throughput_single_node_std_y,
+        throughput_single_node_data[throughput_single_node_data['type'] == 'std']['throughput'],
+        label='std single node')
+    plt.plot(
+        throughput_single_node_std_y,
+        throughput_single_node_data[throughput_single_node_data['type'] == 'buff']['throughput'],
+        label='buff single node')
+    # plt.plot(
+    #          throughput_single_node_std_y,
+    #     throughput_two_nodes_data[throughput_two_nodes_data['type'] == 'std']['throughput'],
+    #          label='std two nodes')
+    # plt.plot(
+    #          throughput_single_node_std_y,
+    #     throughput_two_nodes_data[throughput_two_nodes_data['type'] == 'buff']['throughput'],
+    #          label='buff two nodes')
+    plt.legend()
+    plt.show()
 
 
