68773cea6d199c31fda73e66d698b766c7b17dd6
Kacper Kafara <kacperkafara@gmail.com>
2023-03-19 22:19:07 +0100

    Do not deploy task if build failed

--- lab-02/playground/src/run-ares.sh
+++ lab-02/playground/src/run-ares.sh
@@ -5,6 +5,12 @@ module add .plgrid plgrid/tools/openmpi
 
 # Do not process data, do not archive, do not compile
 make all
+
+if [[ $? -ne 0 ]]
+then
+  exit 1
+fi
+
 sbatch ./run.sh -C -D -Z -S "1 2 3 4 5"
 sbatch ./run.sh -C -D -Z -S "6 7 8 9 10"
 sbatch ./run.sh -C -D -Z -s "1 2 3 4 5"
