e1766bc0b178b46651662d3fb55b5879fc961e77
Kacper Kafara <kacperkafara@gmail.com>
2023-04-01 14:59:10 +0200

    Prepare basic prog

--- openmp/src/main.c
+++ openmp/src/main.c
@@ -1,17 +1,62 @@
-#include <mpi.h>
+#include <omp.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define TIME_SCALE_FACTOR 1e6
+
+typedef uint64_t u64;
+typedef double Data_t;
+
+typedef struct Args {
+  u64 array_size;
+} Args;
+
+Args g_args;
+
+void parse_args(const int argc, char *argv[], Args *out) {
+  assert((argc == 2 && "Single argument is expected"));
+
+  // Assuming happy path here
+  out->array_size = strtoull(argv[1], NULL, 10);
+}
+
+void print_arr(const Data_t *arr, const u64 size){
+  for (u64 i = 0; i < size; ++i) {
+    printf("%lf\n", arr[i]);
+  }
+}
+
 
 int main(int argc, char * argv[]) {
-  int rank, size;
+  srand48(731);
+  parse_args(argc, argv, &g_args);
+  
+  int tid, n_threads;
+  double time_s, time_e;
+  Data_t *data = (Data_t *) malloc(sizeof(Data_t) * g_args.array_size);
 
+  printf("Filling array with random numbers\n");
+  
+  time_s = omp_get_wtime() * TIME_SCALE_FACTOR;
+  #pragma omp parallel private(tid, n_threads)
+  {
+    tid = omp_get_thread_num();
+    printf("Thread %d is executing\n", tid);
 
-	MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (tid == 0) {
+      n_threads = omp_get_num_threads();
+      printf("Total number of used threads: %d\n", n_threads);
+    }
+    #pragma omp for
+    for (u64 i = 0; i < g_args.array_size; ++i) {
+      data[i] = drand48();
+    }
+  }
+  time_e = omp_get_wtime() * TIME_SCALE_FACTOR - time_s;
 
-  printf("Hello world from process %d of %d\n", rank, size);
-
-	MPI_Finalize();
+  printf("Elapsed time: %lf us\n", time_e);
 
 	return 0;
 }
