6bd6168d5a8607eb3631a94863b98362dd0c160b
Kacper Kafara <kacperkafara@gmail.com>
2023-04-18 23:35:46 +0200

    Style changes

--- openmp/src/plot.py
+++ openmp/src/plot.py
@@ -7,10 +7,10 @@ pl.Config.set_tbl_rows(100)
 plt.rcParams['errorbar.capsize'] = 5
 plt.rcParams['figure.figsize'] = (16, 9)
 
-large_mark_size = 100
-small_mark_size = 50
-primary_mark_style = 'o'
-secondary_mark_style = '^'
+LARGE_MARK_SIZE = 100
+SMALL_MARK_SIZE = 50
+PRIMARY_MAKR_STYLE = 'o'
+SECONDARY_MARK_STYLE = '^'
 
 TIME_SCALE_FACTOR = 1e6
 
@@ -28,45 +28,42 @@ data_seq_file = path.Path('data-arch',
                           'ares',
                           # 'final-sync-2023-04-16-17-31-34.csv')
                           # 'final-sync-2023-04-17-03-11-38.csv')
-                          'final-sync-2023-04-17-01-32-48.csv')
+                          # 'final-sync-2023-04-17-01-32-48.csv')
+                          'final-sync-2023-04-17-03-11-38.csv')
 
 assert data_seq_file.is_file(), "Seq data file exists"
 assert data_par_file.is_file(), "Par data file exists"
 
-data_raw_df = (
+col_names = ['total', 'draw', 'scatter', 'sort', 'gather']
+
+# Sequential experiment plots
+
+data_df = (
     pl.scan_csv(data_seq_file, has_header=True)
     .drop(['arrsize', 'nthreads'])
     .select([
         pl.col('total') / TIME_SCALE_FACTOR,
         pl.exclude('total')
     ])
-)
-
-data_df = (
-    data_raw_df.select([
-        pl.all(),
-        pl.col('total').mean().over('bsize').alias('total_mean'),
-        pl.col('total').std().over('bsize').alias('total_std')
-    ])
     .sort(['bsize', 'sid'])
+    .groupby(['bsize'])
+    .agg(
+        [
+            pl.col('total').mean().alias('total_mean'),
+            pl.col('total').std().alias('total_std')
+        ]
+    )
     .collect()
 )
 
 fig, plot = plt.subplots(nrows=1, ncols=1)
 
-series_ids = data_df.get_column('sid').unique().sort()
-x_data = data_df.get_column('bsize').unique().sort()
-
-y_data = data_df.select(pl.col('total_mean').unique().over('bsize')).to_series()
-y_error = data_df.select(pl.col('total_std').unique().over('bsize')).to_series()
-
+x_data = data_df.get_column('bsize')
+y_data = data_df.get_column('total_mean')
+y_error = data_df.get_column('total_std')
 
 plot.errorbar(x_data, y_data, yerr=y_error,
-              marker=primary_mark_style, linestyle=':', label='Średnia')
-
-for series_id in series_ids:
-    plot.scatter(x_data, data_df.filter(pl.col('sid') == series_id).sort('bsize').to_series(),
-                 marker=secondary_mark_style, label=f'Seria {series_id}')
+              marker=PRIMARY_MAKR_STYLE, linestyle=':', label='Średnia')
 
 plot.set(
     title="Czas wykonania alg. sekwencyjnego a rozmiar kubełków, dane: 256MB",
@@ -77,7 +74,7 @@ plot.legend()
 fig.tight_layout()
 fig.savefig(plot_dir.joinpath('seq-256.png'))
 
-col_names = ['total', 'draw', 'scatter', 'sort', 'gather']
+# Parallel experiment plots
 
 data_df = (
     pl.scan_csv(data_par_file, has_header=True)
@@ -98,12 +95,12 @@ y_expected_1 = [total_mean_0 / t for t in x_data]
 fig_par, plot_par = plt.subplots(nrows=1, ncols=1)
 
 plot_par.errorbar(x_data, y_data, yerr=data_df.get_column('total_std'),
-                  marker=primary_mark_style, linestyle='--', label='Czas całkowity')
+                  marker=PRIMARY_MAKR_STYLE, linestyle='--', label='Czas całkowity')
 plot_par.plot(x_data, y_expected_1, linestyle='dashdot', label='$y = t_0 / x$')
 
 for col_name in col_names[1:]:
     plot_par.errorbar(x_data, data_df.get_column(f'{col_name}_mean'), yerr=data_df.get_column(f'{col_name}_std'),
-                      marker=secondary_mark_style, linestyle=':', label=f'{col_name}')
+                      marker=SECONDARY_MARK_STYLE, linestyle=':', label=f'{col_name}')
 
 plot_par.set(
     title="Dane: 256MB, kubełek: 500",
@@ -136,12 +133,12 @@ fig_sp, plot_sp = plt.subplots(nrows=1, ncols=1)
 y_data = total_mean_0 / y_data
 y_expected_sp = [x for x in x_data]
 
-plot_sp.plot(x_data, y_data, marker=primary_mark_style, linestyle='--', label='Średnia')
+plot_sp.plot(x_data, y_data, marker=PRIMARY_MAKR_STYLE, linestyle='--', label='Średnia')
 plot_sp.plot(x_data, y_expected_sp, linestyle='dashdot', label='$y = x$')
 
 for col_name in col_names[1:]:
     data_series = data_df.get_column(f'{col_name}_mean')
-    plot_sp.plot(x_data, data_series.head(1) / data_series, marker=secondary_mark_style,
+    plot_sp.plot(x_data, data_series.head(1) / data_series, marker=SECONDARY_MARK_STYLE,
                  linestyle=':', label=f'{col_name}')
 
 
