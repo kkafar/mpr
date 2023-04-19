0f03ea1adf65361d9b06a0f72e9e675be7289120
Kacper Kafara <kacperkafara@gmail.com>
2023-04-19 18:43:05 +0200

    Make plotting script easier to work with

--- openmp/src/plot.py
+++ openmp/src/plot.py
@@ -5,35 +5,57 @@ from plots.par import process_par_exp
 from plots.seq import processs_seq_exp
 
 
+def deduce_expname_from_filename(filename: str) -> str:
+    expname = filename.split('-')[1]
+    if expname == "async":
+        return "par"
+    elif expname == "sync":
+        return "seq"
+    else:
+        return "unknown"
+
+
+data_dir = path.Path('data-arch', 'ares')
+data_files = {
+    "par": data_dir.joinpath('final-async-2023-04-17-15-17-02.csv'),
+    "seq": data_dir.joinpath('final-sync-2023-04-19-15-00-00.csv')
+}
+
 plot_dir = path.Path('plots', 'sort')
 if not plot_dir.is_dir():
     plot_dir.mkdir(parents=True, exist_ok=True)
-
 assert plot_dir.is_dir(), "Plot directory exists"
 
+if len(sys.argv) == 1:
+    assert data_files["seq"].is_file(), "Seq data file exists"
+    assert data_files["par"].is_file(), "Par data file exists"
 
-data_par_file = path.Path('data-arch',
-                          'ares',
-                          'final-async-2023-04-17-15-17-02.csv')
+    processs_seq_exp(data_files["seq"], plot_dir)
+    process_par_exp(data_files["par"], plot_dir)
 
-data_seq_file = path.Path('data-arch',
-                          'ares',
-                          # 'final-sync-2023-04-16-17-31-34.csv')
-                          # 'final-sync-2023-04-17-03-11-38.csv')
-                          # 'final-sync-2023-04-17-01-32-48.csv')
-                          # 'final-sync-2023-04-17-03-11-38.csv')
-                          'final-sync-2023-04-19-15-00-00.csv')
+if len(sys.argv) == 2:  # path to datafile is expected, we assume parallel experiment
+    data_file = path.Path(sys.argv[1])
+    assert data_file.is_file(), "Data file exists"
 
-assert data_seq_file.is_file(), "Seq data file exists"
-assert data_par_file.is_file(), "Par data file exists"
+    expname = deduce_expname_from_filename(data_file.name)
+    assert expname == "par" or expname == "seq", "Correct exp name"
 
-col_names = ['total', 'draw', 'scatter', 'sort', 'gather']
+    if expname == "par":
+        process_par_exp(data_file, plot_dir)
+    else:
+        processs_seq_exp(data_file, plot_dir)
 
-# Sequential experiment plots
+if len(sys.argv) == 3:  # exptype, datafile
+    expname = sys.argv[1]
+    assert expname == "par" or expname == "seq", "Correct exp name"
 
-processs_seq_exp(data_seq_file, plot_dir)
-process_par_exp(data_par_file, plot_dir)
+    data_file = path.Path(sys.argv[2])
+    assert data_file.is_file(), "Data file exists"
 
+    if expname == "par":
+        process_par_exp(data_file, plot_dir)
+    else:
+        processs_seq_exp(data_file, plot_dir)
 
 plt.show()
 
