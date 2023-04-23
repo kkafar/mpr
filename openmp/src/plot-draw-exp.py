1f05940c161b3e5e1983cda03641d9cdfa19b3da
Kacper Kafara <kacperkafara@gmail.com>
2023-04-23 23:20:46 +0200

    Format plot-draw-exp.py

--- openmp/src/plot-draw-exp.py
+++ openmp/src/plot-draw-exp.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 pl.Config.set_tbl_rows(100)
 plt.rcParams['errorbar.capsize'] = 5
 plt.rcParams['figure.figsize'] = (16, 9)
-largemarksize=100
-smallmarksize=50
-primarymarkstyle='o'
-secondarymarkstyle='^'
+largemarksize = 100
+smallmarksize = 50
+primarymarkstyle = 'o'
+secondarymarkstyle = '^'
 
 plotdir = path.Path('plots')
 if not plotdir.is_dir():
@@ -17,7 +17,7 @@ if not plotdir.is_dir():
 assert plotdir.is_dir(), "Plotdir exists"
 
 
-exctx='vcluster'
+exctx = 'vcluster'
 datafile = path.Path('data-arch', 'draw-exp', 'vcluster-final-2023-04-04-10-10-55.csv')
 # datafile = path.Path('data-arch', 'vcluster-final-2023-04-04-08-59-35.csv')
 # datafile = path.Path('data-arch', 'vcluster-final-2023-04-04-07-35-36.csv')
@@ -27,7 +27,8 @@ datafile = path.Path('data-arch', 'draw-exp', 'vcluster-final-2023-04-04-10-10-5
 
 assert datafile.is_file(), "Datafile exists and is a file"
 
-data_raw = pl.read_csv(datafile, has_header=True).sort(pl.col(['type', 'threads', 'chunk', 'size']))
+data_raw = pl.read_csv(datafile, has_header=True).sort(
+    pl.col(['type', 'threads', 'chunk', 'size']))
 
 # print(data_raw)
 
@@ -55,11 +56,15 @@ for xtype in exptypes:
         fig, (plot_time, plot_sp) = plt.subplots(nrows=1, ncols=2)
 
         data_time = data_mean.filter((pl.col('size') == size) & (pl.col('type') == xtype))
-        data_time_ch_2 = data_time.filter(pl.col('chunk') == '2').sort(pl.col('threads')).select(pl.col('time'))
-        data_time_ch_auto = data_time.filter(pl.col('chunk') == 'auto').sort(pl.col('threads')).select(pl.col('time'))
+        data_time_ch_2 = data_time.filter(pl.col('chunk') == '2').sort(
+            pl.col('threads')).select(pl.col('time'))
+        data_time_ch_auto = data_time.filter(pl.col('chunk') == 'auto').sort(
+            pl.col('threads')).select(pl.col('time'))
         # print(data_time_ch_2)
-        plot_time.plot(threads, data_time_ch_2, label="Chunk: 2", marker=primarymarkstyle, linestyle=':')
-        plot_time.plot(threads, data_time_ch_auto, label="Chunk: auto", marker=secondarymarkstyle, linestyle=':')
+        plot_time.plot(threads, data_time_ch_2, label="Chunk: 2",
+                       marker=primarymarkstyle, linestyle=':')
+        plot_time.plot(threads, data_time_ch_auto, label="Chunk: auto",
+                       marker=secondarymarkstyle, linestyle=':')
 
         strsize = int(size * 8 / 2 ** 20)
         plot_time.set(
@@ -70,12 +75,15 @@ for xtype in exptypes:
 
         t1_ch_2 = data_time_ch_2.to_series()[0]
         t1_ch_auto = data_time_ch_auto.to_series()[0]
-        data_speedup_ch_2 = data_time.filter(pl.col('chunk') == '2').sort(pl.col('threads')).select((t1_ch_2 / pl.col('time')).alias("speedup"))
-        data_speedup_ch_auto = data_time.filter(pl.col('chunk') == 'auto').sort(pl.col('threads')).select((t1_ch_auto / pl.col('time')).alias("speedup"))
+        data_speedup_ch_2 = data_time.filter(pl.col('chunk') == '2').sort(
+            pl.col('threads')).select((t1_ch_2 / pl.col('time')).alias("speedup"))
+        data_speedup_ch_auto = data_time.filter(pl.col('chunk') == 'auto').sort(
+            pl.col('threads')).select((t1_ch_auto / pl.col('time')).alias("speedup"))
 
-        plot_sp.plot(threads, data_speedup_ch_2, label="Chunk: 2", marker=primarymarkstyle, linestyle=":")
-        plot_sp.plot(threads, data_speedup_ch_auto, label="Chunk: auto", marker=secondarymarkstyle, linestyle=":")
-
+        plot_sp.plot(threads, data_speedup_ch_2, label="Chunk: 2",
+                     marker=primarymarkstyle, linestyle=":")
+        plot_sp.plot(threads, data_speedup_ch_auto, label="Chunk: auto",
+                     marker=secondarymarkstyle, linestyle=":")
 
         plot_sp.set(
             title=f"{exctx}, schedule: {xtype}, size: {strsize}MB, skalowanie silne",
