ef93522b66edeb164120f91caa600d331daf3562
Kacper Kafara <kacperkafara@gmail.com>
2023-05-22 19:27:42 +0200

    Improve script

--- mapreduce/run-seq.sh
+++ mapreduce/run-seq.sh
@@ -10,23 +10,46 @@ data_dir="data-mock"
 data_files=("data-1GB.txt" "data-5GB.txt" "data-10GB.txt")
 rust_mapper="rust/mapper/target/release/mapper"
 rust_reducer="rust/reducer/target/release/reducer"
+mode="unknown"
+sizes=("1" "5" "10")
 
 if [[ $# -gt 0 ]]
 then
   data_dir="$1"  
 fi
 
+if [[ $# -gt 1 ]]
+then
+  rust_mapper="$2"  
+fi
+
+if [[ $# -gt 2 ]]
+then
+  rust_reducer="$3"  
+fi
+
+if [[ $# -gt 3 ]]
+then
+  mode="$4"  
+fi
+
 # Workaround to avoid using shell buitin `time` cmd
 time_cmd="$(which time)"
 
-mkdir -p results
-result_file="results/result-$(date +%y-%m-%d-%h-%M-%S).txt"
+mkdir -p results/tmp
+result_file="results/result-$(date +%y-%m-%d-%H-%M-%S).txt"
+tmp_file="results/tmp/tmp-${result-file}"
 
 echo "time,mode,size" > "${result_file}"
 
-for file in "${data_files[@]}"
+for i in $(seq 0 1 "$(( ${#data_files[@]} - 1))")
 do
-  "${time_cmd}" --format="%E" --output "${result_file}" --append cat ${data_dir}/${file} | ${rust_mapper} | ${rust_reducer}
+  file="${data_files[${i}]}"
+  size="${sizes[${i}]}"
+  # echo "Running for file ${file}, size ${size}"
+  "${time_cmd}" --format="%E" --output ${tmp_file} cat ${data_dir}/${file} | ${rust_mapper} | ${rust_reducer} &> /dev/null
+  cat ${tmp_file} | awk -v mode="${mode}" -v size="${size}" -F ',' '{ print $0 "," mode "," size }' >> ${result_file}
 done
 
+rm -fr "results/tmp/"
 
