da6ae3362d3a4789c81e42ff6b94ec54fba8a764
Kacper Kafara <kacperkafara@gmail.com>
2023-05-22 16:57:13 +0200

    Prepare rust mapper & reducer

--- mapreduce/rust/mapper/src/main.rs
+++ mapreduce/rust/mapper/src/main.rs
@@ -1,3 +1,10 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = std::io::BufWriter::new(std::io::stdout());
+    std::io::stdin().lines().for_each(|line| {
+        line.unwrap().split_whitespace().for_each(|word| {
+            stdout.write_fmt(format_args!("{word},1\n"));
+        });
+    });
 }
--- mapreduce/rust/reducer/src/main.rs
+++ mapreduce/rust/reducer/src/main.rs
@@ -1,3 +1,30 @@
+use std::{collections::HashMap, io::Write};
+
+// This program assumes that stdin is sorted!!
 fn main() {
-    println!("Hello, world!");
+    let mut wtc: HashMap<String, usize> = HashMap::new();
+
+    let mut stdout = std::io::BufWriter::new(std::io::stdout());
+    std::io::stdin()
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            let split = line.split_once(',').unwrap();
+            (split.0.to_owned(), split.1.to_owned())
+            // let word = split.0.to_owned();
+            // let count = split.1.to_owned();
+            // (word, count)
+        })
+        .map(|(word, str_count)| {
+            (word, str_count.parse::<usize>().unwrap()) 
+        })
+        .for_each(|(word, count)| {
+            if let Some(crt_count) = wtc.get_mut(&word) {
+                *crt_count += count; 
+            } else {
+                wtc.insert(word, count);
+            }
+        });
+
+    wtc.iter().for_each(|(word, count)| { stdout.write_fmt(format_args!("{word},{count}\n")); });
 }
