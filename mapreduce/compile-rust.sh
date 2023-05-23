d94ef2c43f383947bee4b8ce887a2884a6413e2a
Kacper Kafara <kacperkafara@gmail.com>
2023-05-23 08:34:37 +0200

    Install rust binaries after build

--- mapreduce/compile-rust.sh
+++ mapreduce/compile-rust.sh
@@ -3,6 +3,11 @@
 root_dir=$(pwd)
 cd rust/mapper
 cargo build --release
+cargo install --root ${root_dir} --path .
+
 cd ../reducer
 cargo build --release
+cargo install --root ${root_dir} --path .
 cd ${root_dir}
+
+
