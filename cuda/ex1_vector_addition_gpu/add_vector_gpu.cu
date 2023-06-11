703391c76c5b3e9591d94c0e9454e3c164b1baec
Kacper Kafara <kacperkafara@gmail.com>
2023-06-11 16:43:52 +0200

    Try to run experiments in a loop

--- cuda/ex1_vector_addition_gpu/add_vector_gpu.cu
+++ cuda/ex1_vector_addition_gpu/add_vector_gpu.cu
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
 
-#include "gputimer.h"
+// #include "gputimer.h"
+struct GpuTimer {
+  cudaEvent_t start;
+  cudaEvent_t stop;
+
+  GpuTimer() {
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+  }
+
+  ~GpuTimer() {
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+  }
+
+  void Start() { cudaEventRecord(start, 0); }
+
+  void Stop() { cudaEventRecord(stop, 0); }
+
+  float Elapsed() {
+    float elapsed;
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed, start, stop);
+    return elapsed;
+  }
+};
 
 #define N 1048576 // 512, 1024, ... 16384, 32768, 65536, 131072
 
@@ -27,48 +54,61 @@ void print_output(int *a, int *b, int *c) {
     printf("\n %d + %d  = %d", a[idx], b[idx], c[idx]);
 }
 int main(void) {
+  std::cout << "Starting the run...\n";
   int *a, *b, *c;
   int *d_a, *d_b, *d_c; // device copies of a, b, c
   int threads_per_block = 0, no_of_blocks = 0;
   GpuTimer timer;
 
-  int size = N * sizeof(int);
+  std::vector<int> arr_sizes;
+  int base = 512;
+  while (base <= 131072) {
+    arr_sizes.push_back(base);
+    base *= 2;
+  }
 
-  // Alloc space for host copies of a, b, c and setup input values
-  a = (int *)malloc(size);
-  fill_array(a);
-  b = (int *)malloc(size);
-  fill_array(b);
-  c = (int *)malloc(size);
+  printf("arrsize,nblocks,time\n");
 
-  // Alloc space for device copies of a, b, c
-  cudaMalloc((void **)&d_a, size);
-  cudaMalloc((void **)&d_b, size);
-  cudaMalloc((void **)&d_c, size);
+  for (int i = 0; i < arr_sizes.size(); ++i) {
+    int size = arr_sizes[i] * sizeof(int);
 
-  // Copy inputs to device
-  cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    // Alloc space for host copies of a, b, c and setup input values
+    a = (int *)malloc(size);
+    fill_array(a);
+    b = (int *)malloc(size);
+    fill_array(b);
+    c = (int *)malloc(size);
 
-  threads_per_block = 512;
-  no_of_blocks = N / threads_per_block;
-  timer.Start();
-  device_add<<<no_of_blocks, threads_per_block>>>(d_a, d_b, d_c);
-  timer.Stop();
+    // Alloc space for device copies of a, b, c
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_b, size);
+    cudaMalloc((void **)&d_c, size);
 
-  // Copy result back to host
-  cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    // Copy inputs to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-  // print_output(a,b,c);
-  printf("N = %d; no_of_blocks = %d; Elapsed time = %f ms\n", N, no_of_blocks,
-         timer.Elapsed());
+    threads_per_block = 512;
+    no_of_blocks = arr_sizes[i] / threads_per_block;
+    timer.Start();
+    device_add<<<no_of_blocks, threads_per_block>>>(d_a, d_b, d_c);
+    timer.Stop();
 
-  free(a);
-  free(b);
-  free(c);
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+    // print_output(a,b,c);
+    printf("%d,%d,%f\n", arr_sizes[i], no_of_blocks, timer.Elapsed());
+    // printf("N = %d; no_of_blocks = %d; Elapsed time = %f ms\n", N, no_of_blocks,
+    //        timer.Elapsed());
+
+    free(a);
+    free(b);
+    free(c);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+  }
 
   return 0;
 }
