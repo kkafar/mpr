9db3d5a11cb4e6f8ed638db0bebf1114519d67e0
Kacper Kafara <kacperkafara@gmail.com>
2023-06-17 14:47:38 +0200

    Renade directories

--- cuda-2/1 Reduction/profiler.bat
+++
@@ -1,1 +0,0 @@
-nvprof -o reduction_global_gpu.nvvp reduction_global_gpu.exe
--- cuda-2/1 Reduction/reduction_global_gpu.bat
+++
@@ -1,1 +0,0 @@
-nvcc -o reduction_global_gpu reduction_global.cpp reduction_global_kernel.cu -arch=sm_50 -allow-unsupported-compiler
